62443394f53ea4d56dcde879c0fa71c0d06f0449
Alex Tim <buildmnt@gmail.com>
2022-09-04 21:06:09 +0300

    chore: example deps

--- examples/crud/vite.config.ts
+++ examples/crud/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsonServer from 'vite-plugin-simple-json-server';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [svgr(), react(), jsonServer()],
+  plugins: [react(), jsonServer()],
   build: {
     minify: false,
   },
